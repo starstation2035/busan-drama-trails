8bec8de9ed35a08eef76ea747dc1350bb698340f
Star <spacestation2035@gmail.com>
2026-05-18 18:50:04 +0900

    Fix broken image paths in mockReviews.ts

--- src/data/mockReviews.ts
+++ src/data/mockReviews.ts
@@ -17,7 +17,7 @@ export const MOCK_REVIEWS: Review[] = [
     author: "Mei-Ling",
     location: "Haeundae Beach",
     spotId: "haeundae-beach",
-    image: "/community_review_haeundae_1776481959233.png",
+    image: "/images/spots/haeundae.png",
     content:
       "The sunset here is absolutely magical. Just like in the dramas! Make sure to visit around 5 PM for the best lighting. 🌅",
     likes: 124,
@@ -29,7 +29,7 @@ export const MOCK_REVIEWS: Review[] = [
     author: "Seung-jin",
     location: "Gamcheon Culture Village",
     spotId: "gamcheon-village",
-    image: "/community_review_gamcheon_1776481981172.png",
+    image: "/images/posters/gamcheon.jpg",
     content:
       "Don't miss the Little Prince statue! The view of the colorful houses is even better in person. Best photo spot in Busan! 📸",
     likes: 89,
@@ -41,7 +41,7 @@ export const MOCK_REVIEWS: Review[] = [
     author: "Yuki",
     location: "Gwangalli Beach",
     spotId: "gwangalli-beach",
-    image: "/busan_harbor_bridge_sunset_1776481887078.png",
+    image: "/images/spots/gwangalli.png",
     content:
       "The night view of the bridge is stunning! A perfect place for a romantic walk after dinner. 🌃",
     likes: 210,
